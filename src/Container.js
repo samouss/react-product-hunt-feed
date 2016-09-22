201b0f30f5670fe68114be8303d50df84a8f9e35
Samuel Vaillant <samuel.vllnt@gmail.com>
2016-09-22 14:57:51 +0200

    WIP

--- src/Container.js
+++ src/Container.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
+import { getCategories, getPosts } from './services/api';
+import { normalizePost } from './services/posts';
 
 /**
  * @name Container
  */
 export default class Container extends Component {
 
+  /**
+   * @constructor
+   */
   constructor(props) {
     super(props);
 
-    this.state = { message: '<Container />' };
+    this.state = {
+      isLoading: true,
+      categories: [],
+      posts: [],
+    };
   }
 
+  /**
+   * @name componentDidMount
+   */
+  componentDidMount() {
+    Promise.all([
+      getCategories(),
+      getPosts({ query: { per_page: 10 } }),
+    ]).then(([res1, res2]) => {
+      this.setState({
+        isLoading: false,
+        categories: this.state.categories.concat(res1.categories),
+        posts: this.state.posts.concat(res2.posts.map(normalizePost)),
+      });
+    });
+  }
+
+  /**
+   * @name render
+   */
   render() {
+    const { isLoading, posts } = this.state;
+
+    if (isLoading) {
+      return (
+        <div className="container">
+          <h1>Product Hunt</h1>
+
+          <p>Loading....</p>
+        </div>
+      );
+    }
+
     return (
       <div className="container">
-        <h1>React starter</h1>
+        <h1>Product Hunt</h1>
 
-        Hello from {this.state.message} !!
+        {posts.map(post => (
+          <div key={post.id}>{post.title}</div>
+        ))}
       </div>
     );
   }
