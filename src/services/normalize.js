1462b5ceb618350b20d667c526e39f4c998b57db
Samuel Vaillant <samuel.vllnt@gmail.com>
2016-09-23 16:16:27 +0200

    refactor: merge posts and normalize

--- src/services/normalize.js
+++ src/services/normalize.js
@@ -7,3 +7,29 @@ export function byId(state, items) {
 export function getIds(state, items) {
   return state.concat(items.map(item => item.id));
 }
+
+export function post(post) {
+  return {
+    id: post.id,
+    categoryId: post.category_id,
+    title: post.name,
+    description: post.tagline,
+    createdAt: post.created_at,
+    link: post.discussion_url,
+    votes: post.votes_count,
+    topics: post.topics.map(topic),
+    thumbnail: post.thumbnail.image_url,
+  };
+}
+
+export function topic({ name, ...rest }) {
+  return { ...rest, title: name };
+}
+
+export function category(category) {
+  return {
+    id: category.id,
+    title: category.name,
+    color: category.color,
+  };
+}
--- src/services/normalize.spec.js
+++ src/services/normalize.spec.js
@@ -90,4 +90,100 @@ describe('normalize', () => {
       result.should.be.deep.equal(expectation);
     });
   });
+
+  /**
+   * @name post
+   */
+  describe('post', () => {
+    it('should return a post normalized from API object', () => {
+      const post = {
+        id: 76884,
+        category_id: 1,
+        name: 'Moojis',
+        tagline: 'Changing the face of Emojis',
+        created_at: '2016-09-22T04:59:49.893-07:00',
+        discussion_url: 'http://producthunt/discussion',
+        votes_count: 1,
+        topics: [
+          { id: 8, name: 'iPhone', slug: 'iphone' },
+          { id: 351, name: 'iMessage Apps', slug: 'imessage-apps' },
+          { id: 45, name: 'Emoji', slug: 'emoji' },
+        ],
+        thumbnail: {
+          id: 255139,
+          media_type: 'image',
+          image_url: 'http://producthunt/thumbnail',
+          metadata: {},
+        },
+      };
+
+      const expectation = {
+        id: 76884,
+        categoryId: 1,
+        title: 'Moojis',
+        description: 'Changing the face of Emojis',
+        createdAt: '2016-09-22T04:59:49.893-07:00',
+        link: 'http://producthunt/discussion',
+        votes: 1,
+        topics: [
+          { id: 8, title: 'iPhone', slug: 'iphone' },
+          { id: 351, title: 'iMessage Apps', slug: 'imessage-apps' },
+          { id: 45, title: 'Emoji', slug: 'emoji' },
+        ],
+        thumbnail: 'http://producthunt/thumbnail',
+      };
+
+      const result = NormalizeModule.post(post);
+
+      result.should.be.deep.equal(expectation);
+    });
+  });
+
+  /**
+   * @name topic
+   */
+  describe('topic', () => {
+    it('should return a topic normalized from API object', () => {
+      const post = {
+        id: 8,
+        name: 'iPhone',
+        slug: 'iphone',
+      };
+
+      const expectation = {
+        id: 8,
+        title: 'iPhone',
+        slug: 'iphone',
+      };
+
+      const result = NormalizeModule.topic(post);
+
+      result.should.be.deep.equal(expectation);
+    });
+  });
+
+  /**
+   * @name category
+   */
+  describe('category', () => {
+    it('should return a category normalized from API object', () => {
+      const post = {
+        id: 1,
+        slug: 'tech',
+        name: 'Tech',
+        item_name: 'product',
+        color: '#5898f1',
+      };
+
+      const expectation = {
+        id: 1,
+        title: 'Tech',
+        color: '#5898f1',
+      };
+
+      const result = NormalizeModule.category(post);
+
+      result.should.be.deep.equal(expectation);
+    });
+  });
 });
