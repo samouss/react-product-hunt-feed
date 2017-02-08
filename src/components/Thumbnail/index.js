8e5c26c63d909b03a9c8ad9a9fb4093c8c4724a9
Samuel Vaillant <samuel.vllnt@gmail.com>
2017-02-08 19:28:54 +0100

    feat(Thumbnail): add alt property via title

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -1,17 +1,18 @@
 import React, { PropTypes } from 'react';
 import { applyQueryParameters } from '../../core/http';
 
-export default function Thumbnail({ source, width, height }) {
+export default function Thumbnail({ source, title, width, height }) {
   return (
     <img
       src={applyQueryParameters(source, { w: width, h: height })}
-      role="presentation"
+      alt={title}
     />
   );
 }
 
 Thumbnail.propTypes = {
   source: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
 };
--- src/components/Thumbnail/index.spec.js
+++ src/components/Thumbnail/index.spec.js
@@ -14,6 +14,7 @@ describe('<Thumbnail />', () => {
   it('should render correctly with given props', sinon.test(function test() {
     const applyQueryParameters = this.stub(HttpModule, 'applyQueryParameters');
     const source = 'http://static.dev';
+    const title = 'Some title';
     const width = 40;
     const height = 50;
 
@@ -24,16 +25,17 @@ describe('<Thumbnail />', () => {
     const expectation = (
       <img
         src="http://static.dev?w=40&h=50"
-        role="presentation"
+        alt="Some title"
       />
     );
 
     const wrapper = shallow(
       <Thumbnail
         source={source}
+        title={title}
         width={width}
         height={height}
-      />
+      />,
     );
 
     wrapper.containsMatchingElement(expectation).should.be.equal(true);
